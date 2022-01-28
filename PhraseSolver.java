e9acfb551e1b39d67a86e419c23eb81891885bde
Brendan_A <56183420+A-I-SmartGuy@users.noreply.github.com>
2022-01-28 12:45:37 -0800

    Add files via upload

--- PhraseSolver.java
+++ PhraseSolver.java
@@ -12,10 +12,25 @@ public class PhraseSolver
   private Player player2;
   private Board game;
   private boolean solved;
+  private int currentPlayer;
+  private Scanner input;
+  private boolean correct;
+  private int total1;
+  private int total2;
+  private Board board;
+
 
   /* your code here - constructor(s) */ 
   public PhraseSolver() {
-
+    solved = false;
+    currentPlayer = 1;
+    input = new Scanner(System.in);
+    correct = true;
+    player1 = new Player("1");
+    player2 = new Player("2");
+    total1 = 0;
+    total2 = 0;
+    board = new Board();
   }
 
   /* your code here - accessor(s) */
@@ -24,20 +39,8 @@ public class PhraseSolver
 
   public void play()
   {
-    boolean solved = false;
-    int currentPlayer = 1;
-    Scanner input = new Scanner(System.in);
-    boolean correct = true;
 
-    Player player1 = new Player("1");
-    Player player2 = new Player("2");
-
-    int total1 = 0;
-    int total2 = 0;
     System.out.println("Welcome, " + player1.getName() + " and " + player2.getName() + ", here is the outline of the phrase:");
-    Board board = new Board();
-    boolean gameLoop = true;
-    String finalGuess = "";
 
     while (!solved) 
   {
@@ -49,7 +52,20 @@ public class PhraseSolver
         total1+=board.getLetterValue();
         System.out.println("Nice job! You got: " + board.getLetterValue() + " points. " + total1 + " in total now for " + player1.getName() +".\n");
       } else {
-        System.out.println("Sorry, that is not in the phrase." + total1 + " in total now for " + player1.getName() +".\n");
+        System.out.println("Sorry, that is not in the phrase. " + total1 + " in total now for " + player1.getName() +".\n");
+      }
+
+      if(board.isSolved()) {
+        solved = true;
+        System.out.println("\n\n");
+        if(total1 > total2) {
+          System.out.println("Congrats! You guys solved the phrase, and " + player1.getName() +" won with a " + (total1 - total2) + " point lead!    " + player1.getName() + ": " + total1 + "    " + player2.getName() + ": " + total2);
+        } else if (total2 > total1) {
+          System.out.println("Congrats! You guys solved the phrase, and " + player2.getName() +" won with a " + (total2 - total1) + " point lead!    " + player1.getName() + ": " + total1 + "    " + player2.getName() + ": " + total2);
+        } else {
+          System.out.println("No way! " + player1.getName() + " and " + player2.getName() + " tied at a score of " + total1 + "!");
+        }
+        return;
       }
 
       System.out.println(board.getSolvedPhrase());
@@ -60,24 +76,21 @@ public class PhraseSolver
         total2+=board.getLetterValue();
         System.out.println("Nice job! You got: " + board.getLetterValue() + " points. " + total2 + " in total now for " + player2.getName() +".\n");
       } else {
-        System.out.println("Sorry, that is not in the phrase." + total2 + " in total now for " + player2.getName() +".\n");
+        System.out.println("Sorry, that is not in the phrase. " + total2 + " in total now for " + player2.getName() +".\n");
       }
-
-      System.out.println(board.getSolvedPhrase() + " <-- get solved    :    get phrase --> " + board.getPhrase());
-
-      /* your code here - game logic */
       
       /* your code here - determine how game ends */
       if(board.isSolved()) {
         solved = true;
         System.out.println("\n\n");
         if(total1 > total2) {
-          System.out.println("Congrats! You guys solved the phrase, and " + player1.getName() +" won with a " + (total1 - total2) + " point leed!  " + player1.getName() + ": " + total1 + "    " + player2.getName() + ": " + total2);
+          System.out.println("Congrats! You guys solved the phrase, and " + player1.getName() +" won with a " + (total1 - total2) + " point lead!\n" + player1.getName() + ": " + total1 + "    " + player2.getName() + ": " + total2);
         } else if (total2 > total1) {
-          System.out.println("Congrats! You guys solved the phrase, and " + player2.getName() +" won with a " + (total2 - total1) + " point leed!" + player1.getName() + ": " + total1 + "    " + player2.getName() + ": " + total2);
+          System.out.println("Congrats! You guys solved the phrase, and " + player2.getName() +" won with a " + (total2 - total1) + " point lead!\n" + player1.getName() + ": " + total1 + "    " + player2.getName() + ": " + total2);
         } else {
           System.out.println("No way! " + player1.getName() + " and " + player2.getName() + " tied at a score of " + total1 + "!");
         }
+        return;
       }
     } 
   }
